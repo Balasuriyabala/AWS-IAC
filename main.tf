d8a412f3691810a6fed0ff15f495e271c2f8f2b7
Balasuriyabala <balasuriyamech15@gmail.com>
2025-05-03 18:12:54 +0530

    vpc

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "S3" {
 module "vpc" {
   source = "./modules/vpc"
   cidr_block = "10.0.0.0/16"
-  availability_zones = data.aws_availability_zones.available.names
+  availability_zones = data.aws_availability_zones.available.names 
   webapp_public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
   webapp_private_subnet_cidr_blocks = ["10.0.101.0/24", "10.0.102.0/24"]
   
